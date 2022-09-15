2fd483a7d5188abeeb5a6e58aae780a5839ef360
Art Schedrov <44718441+artschedrov@users.noreply.github.com>
2022-09-15 21:59:42 +0300

    Update mips_and.asm

--- and/mips_and.asm
+++ and/mips_and.asm
@@ -4,25 +4,25 @@
 
 main:
 	addiu 	$sp,$sp, -32
-	sw	    $fp,28($sp)
+	sw	$fp,28($sp)
 	move    $fp,$sp
 	
 	li      $v0,100		#1100100           
-    sw      $v0,8($fp)
-    li      $v0,120		#1111000
-    sw      $v0,12($fp)
+    	sw      $v0,8($fp)
+    	li      $v0,120		#1111000
+    	sw      $v0,12($fp)
         
-	lw	    $v1,8($fp)
-	lw	    $v0,12($fp)
+	lw	$v1,8($fp)
+	lw	$v0,12($fp)
 	nop
 	and 	$v0,$v1,$v0
-	sw 	    $v0,16,($fp)	#1100000
+	sw	$v0,16,($fp)	#1100000
 	move	$v0,$zero
 	
 	move	$sp,$fp
-	lw	    $fp,28($sp)
+	lw	$fp,28($sp)
 	addiu	$sp,$sp,32
-	jr	    $ra
+	jr	$ra
 	nop
 	
 	
