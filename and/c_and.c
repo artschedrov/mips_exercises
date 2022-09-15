fc3de50998347f3de10bd3ed336ae7460ba73307
Art Schedrov <a.sch@tutanota.com>
2022-09-15 21:57:30 +0300

    побитовое И

---
+++ and/c_and.c
@@ -0,0 +1,6 @@
+int main() {
+    int a, b, c;
+    a = 100;
+    b = 120;
+    c = a & b;
+}
---
+++ and/mips_and.asm
@@ -0,0 +1,28 @@
+# первое значение 1100100
+# второе значение 1111000
+# при побитовом И получаем 1100000
+
+main:
+	addiu 	$sp,$sp, -32
+	sw	    $fp,28($sp)
+	move    $fp,$sp
+	
+	li      $v0,100		#1100100           
+    sw      $v0,8($fp)
+    li      $v0,120		#1111000
+    sw      $v0,12($fp)
+        
+	lw	    $v1,8($fp)
+	lw	    $v0,12($fp)
+	nop
+	and 	$v0,$v1,$v0
+	sw 	    $v0,16,($fp)	#1100000
+	move	$v0,$zero
+	
+	move	$sp,$fp
+	lw	    $fp,28($sp)
+	addiu	$sp,$sp,32
+	jr	    $ra
+	nop
+	
+	
